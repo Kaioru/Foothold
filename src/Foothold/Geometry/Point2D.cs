3cd47fd7ceee7e0715ba22ac303b5e13cbe2de84
Kaioru <keith@kaioru.co>
2022-07-10 18:05:02 +0800

    Initial commit 😛

---
+++ src/Foothold/Geometry/Point2D.cs
@@ -0,0 +1,7 @@
+public struct Point2D
+{
+    public int X { get; }
+    public int Y { get; }
+
+    public Point2D(int x, int y) { X = x; Y = y; }
+}
---
+++ src/Foothold/Geometry/Segment2D.cs
@@ -0,0 +1,7 @@
+public struct Segment2D
+{
+    public Point2D P1 { get; }
+    public Point2D P2 { get; }
+
+    public Segment2D(Point2D p1, Point2D p2) { P1 = p1; P2 = p2; }
+}
