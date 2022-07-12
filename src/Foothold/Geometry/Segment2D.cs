56b3be62ca15b042a9ed12abc51cb7d35c32ca38
Kaioru <keith@kaioru.co>
2022-07-12 15:58:16 +0800

    Add Length property to Segment2D

--- src/Foothold/Geometry/Segment2D.cs
+++ src/Foothold/Geometry/Segment2D.cs
@@ -9,6 +9,7 @@ public struct Segment2D
 
     public Point2D Middle => new((P1.X + P2.X) / 2, (P1.Y + P2.Y) / 2);
 
+    public double Length => P1.Distance(P2);
     public double Slope => IsVertical ? 0 : (P2.Y - P1.Y) / (P2.X - P1.X);
     public bool IsVertical => P1.X == P1.X;
     public bool IsHorizontal => P1.Y == P2.Y;
