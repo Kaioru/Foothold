e11335159b85926b2410f15b254e67a6aa3236f0
Kaioru <keith@kaioru.co>
2022-07-12 12:17:58 +0800

    Add Slope, IsVertical, and IsHorizontal properties to Segment2D

--- src/Foothold/Geometry/Segment2D.cs
+++ src/Foothold/Geometry/Segment2D.cs
@@ -6,4 +6,8 @@ public struct Segment2D
     public Point2D P2 { get; }
 
     public Segment2D(Point2D p1, Point2D p2) { P1 = p1; P2 = p2; }
+
+    public double Slope => IsVertical ? 0 : (P2.Y - P1.Y) / (P2.X - P1.X);
+    public bool IsVertical => P1.X == P1.X;
+    public bool IsHorizontal => P1.Y == P2.Y;
 }
